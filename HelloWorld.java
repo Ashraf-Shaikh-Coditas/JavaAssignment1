475672199343560581950895d122dc3c63576add
Ashraf-Coditas <ashraf.shaikh@coditas.com>
2022-08-17 17:38:15 +0530

    Second Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,3 +3,10 @@ class HelloWorld {
 		System.out.println("Hello World!!!");
 	}	
 }
+
+/*
+C:\Users\Coditas\Assignments\Week2\Assignment 1>javac HelloWorld.java
+
+C:\Users\Coditas\Assignments\Week2\Assignment 1>java HelloWorld
+Hello World!!!
+*/
